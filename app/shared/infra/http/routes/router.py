04cd9e68da0c72bfa0cc4a3e3c39b053f43f26cb
Lucas Bittencourt <lucascafieiro@hotmail.com>
2024-04-07 15:23:34 -0300

    feat: created routing for the Contacts endpoint

--- app/shared/infra/http/routes/router.py
+++ app/shared/infra/http/routes/router.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from app.contacts.endpoints.routes.contacts_routes import contacts_router
 
 
 root_router = APIRouter()
+
+root_router.include_router(contacts_router)
