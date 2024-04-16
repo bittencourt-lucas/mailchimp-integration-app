7b3b8d4f99dcd7256f70b6f3ce15a517e94450a2
Lucas Bittencourt <lucascafieiro@hotmail.com>
2024-04-16 07:40:49 -0300

    test: new tests for MockAPI client

---
+++ tests/infrastructure/test_mockapi_client.py
@@ -0,0 +1,46 @@
+import os
+import pytest
+from src.infrastructure.externals.mockapi_client import MockAPIClient
+
+
+def test_mockapi_client_init(mocker):
+    url = 'https://challenge.trio.dev/api/v1'
+    mocker.patch.dict(os.environ, {
+        'MOCKAPI_BASE_URL': url
+        })
+
+    service = MockAPIClient()
+
+    assert service.base_url == url
+    assert service.client.url == url
+    assert service.client.headers == {'Content-Type': 'application/json'}
+
+
+def test_mockapi_client_missing_base_url(mocker):
+    mocker.patch.dict(os.environ, {
+        'MOCKAPI_BASE_URL': ''
+    })
+
+    with pytest.raises(ValueError):
+        MockAPIClient()
+
+
+def test_format_contacts():
+    service = MockAPIClient()
+    contacts = [{
+            'createdAt': '2022-02-18T16:32:23.057Z',
+            'firstName': 'John',
+            'lastName': 'Doe',
+            'email': 'johndoe@outlook.com',
+            'avatar': 'https://cdn.fakercloud.com/avatars/dshster_128.jpg',
+            'id': '115'
+        }]
+
+    expected_output = [{
+            'firstName': 'John',
+            'lastName': 'Doe',
+            'email': 'johndoe@outlook.com'
+        }]
+
+    output = service.format_contacts(contacts)
+    assert output == expected_output
