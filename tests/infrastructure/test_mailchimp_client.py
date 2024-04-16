b801f517f1c0e570adc99dd1ececa6276c4e182a
Lucas Bittencourt <lucascafieiro@hotmail.com>
2024-04-16 07:52:22 -0300

    test: added tests to see if Mailchimp API Client fails properly when missing env vars

--- tests/infrastructure/test_mailchimp_client.py
+++ tests/infrastructure/test_mailchimp_client.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 from src.infrastructure.externals.mailchimp_api_client \
     import MailchimpAPIClient
 
@@ -19,3 +20,23 @@ def test_mailchimp_api_init(mocker):
         'Authorization': f'Bearer {os.getenv('MAILCHIMP_API_KEY')}',
         }
     assert client.client.client.url == url
+
+
+def test_mailchimp_api_missing_base_url(mocker):
+    mocker.patch.dict(os.environ, {
+        'MAILCHIMP_BASE_URL': '',
+        'MAILCHIMP_API_KEY': 'test_key'
+        })
+
+    with pytest.raises(ValueError):
+        MailchimpAPIClient()
+
+
+def test_mailchimp_api_missing_api_key(mocker):
+    mocker.patch.dict(os.environ, {
+        'MAILCHIMP_BASE_URL': 'https://us1.api.mailchimp.com/3.0',
+        'MAILCHIMP_API_KEY': ''
+        })
+
+    with pytest.raises(ValueError):
+        MailchimpAPIClient()
